e3cac114b072ebd23159e7247dbc7c519e5209d5
bbsharps <billybriarsharps@gmail.com>
2025-10-08 13:26:43 +0100

    add paddle, add pladdle to play, add paddle script, add paddle tests, update ignore for addons

---
+++ scripts/paddle/paddle_script.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+var speed: float = 1
+
+func _ready() -> void:
+	pass
+	
+func _physics_process(_delta: float)->void:					
+	move_and_slide()
+	
+func _process(_delta: float)->void:
+	self.velocity.x = 0
+	
+	match Input.is_action_pressed("ui_left") :
+		true:
+			match Input.is_action_pressed("ui_right"):
+				true:
+					self.velocity.x = 0
+				false: 
+					self.velocity.x = -200
+												
+	match Input.is_action_pressed("ui_right"):
+		true:
+			match Input.is_action_pressed("ui_left"):
+				true:
+					self.velocity.x = 0
+				false:
+					self.velocity.x = 200
+					
+	
